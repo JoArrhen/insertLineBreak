66615ba3879e99fac264e49592a4985c2b98aa52
Joarrhen <arrhen.johan@gmail.com>
2020-07-12 13:32:46 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//document.getElementsById("inputTextArea").addEventListener("click", displayDate);
+function inputDetected() {
+    var input = document.getElementById("inputTextArea").value;
+    var sentnsArr = input.split('\n');
+
+    var result = "";
+    for(i = 0; i < sentnsArr.length; i++) {
+        result += sentnsArr[i];
+        if (i != sentnsArr.length - 1) {
+            result += "\\n";
+        }
+    }
+
+    document.getElementById("outputTextArea").value = result;
+}
